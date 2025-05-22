4e5b9ea1f1d651a983332156412f76b87b4eb3d3
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-05-22 12:21:41 +0200

    Emergency changes done

--- Servidor/index.js
+++ Servidor/index.js
@@ -2,20 +2,25 @@ const port = 8000;
 
 const cors = require("cors");
 const express = require("express");
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerDocument = require("./swagger-output.json");
+
+//const swaggerUi = require("swagger-ui-express");
+//const swaggerDocument = require("./swagger-output.json");
+
 const { dbConnection } = require("./db");
 
+const usersRoutes = require("./routes/users.routes");
 const votesRoutes = require("./routes/votes.routes");
 const pollsRoutes = require("./routes/polls.routes");
+
 const main = () => {
     const app = express();
     app.use(cors());
     app.use(express.json());
 
-    app.use("/", pollsRoutes);
+    app.use("/", usersRoutes);
     app.use("/votes", votesRoutes);
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    app.use("/polls", pollsRoutes);
+    //app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     dbConnection();
     app.listen(port, () => {
