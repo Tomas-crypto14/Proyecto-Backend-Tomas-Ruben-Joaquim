3acb60e2b518079d41818a7a7cd43ab03ca8d9e7
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-05-22 11:43:29 +0200

    commit corrected

--- Servidor/index.js
+++ Servidor/index.js
@@ -6,7 +6,7 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger-output.json");
 const { dbConnection } = require("./db");
 
-// const votesRoutes = require("./routes/votes.routes");
+const votesRoutes = require("./routes/votes.routes");
 const pollsRoutes = require("./routes/polls.routes");
 const main = () => {
     const app = express();
@@ -14,7 +14,7 @@ const main = () => {
     app.use(express.json());
 
     app.use("/", pollsRoutes);
-    // app.use("/votes", votesRoutes);
+    app.use("/votes", votesRoutes);
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     dbConnection();
